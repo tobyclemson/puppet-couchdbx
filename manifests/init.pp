76d048dbe7deb3922ef7db38368795623d2c86cc
Toby Clemson <tobyclemson@gmail.com>
2014-06-15 23:57:12 -0400

    Fix package name to match expectations of compressed_app provider.

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,7 @@
 #
 #   include couchdbx
 class couchdbx {
-  package { 'CouchDBX':
+  package { 'Apache CouchDB':
     provider => 'compressed_app',
     source   => 'http://mirror.metrocast.net/apache/couchdb/binary/mac/1.3.0/Apache-CouchDB.zip'
   }
--- spec/classes/couchdbx_spec.rb
+++ spec/classes/couchdbx_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'couchdbx' do
   it do
-    should contain_package('CouchDBX').with({
+    should contain_package('Apache CouchDB').with({
       :provider => 'compressed_app',
       :source   => 'http://mirror.metrocast.net/apache/couchdb/binary/mac/1.3.0/Apache-CouchDB.zip',
     })
